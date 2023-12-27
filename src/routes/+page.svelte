cc309a25ee813e58586a2187df3d0c51395f9806
Fabio Sousa Vieira <fabsousa23.fs@gmail.com>
2023-12-27 21:34:14 +0200

    breathe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	class="w-full min-h-screen flex flex-col flex-1 max-w-xl *:flex-grow border-2 border-blue-600 *:border-t-blue-600 [&>*:not(:first-child)]:border-t-2"
+	class="mx-2 min-h-screen flex flex-col flex-1 max-w-xl *:flex-grow border-2 border-blue-600 *:border-t-blue-600 [&>*:not(:first-child)]:border-t-2"
 >
 	<Heading extraBlocks>Characters</Heading>
 	{#each characters as character}
